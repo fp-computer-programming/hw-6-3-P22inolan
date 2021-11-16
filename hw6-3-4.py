b5daae9ff05791bc1999ee3c109f00f50578a5b8
Ian Nolan <p22inolan@fairfieldprep.org>
2021-11-15 23:58:40 -0500

    Submit 6-3

--- hw6-3-4.py
+++ hw6-3-4.py
@@ -1,3 +1,16 @@
 # Author: IBN (AMDG) 11/15/2021
 
-number_lst = input("Give me a list of number or letters separated by spaces.")
+number = input("Give me a list of number or letters separated by spaces. ")
+number_lst = number.split()
+
+question = input("Do you want the middle or ends of your list? ")
+
+if question == "ends":
+    ends_lst = number_lst.copy()
+    del ends_lst[1:-1]
+    print(ends_lst)
+else:
+    middle_lst = number_lst.copy()
+    del middle_lst[0]
+    del middle_lst[-1]
+    print(middle_lst)
